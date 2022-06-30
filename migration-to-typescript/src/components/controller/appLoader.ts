763fa91c108d331171eb81333260dd34f20e74eb
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-07-01 01:57:46 +0300

    feat: add api key to app loader

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ba946fe01f894581840ea4bd6dcfa0b9', // получите свой ключ https://newsapi.org/
         });
     }
 }
